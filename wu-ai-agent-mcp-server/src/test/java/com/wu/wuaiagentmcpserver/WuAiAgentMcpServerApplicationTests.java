e46a3adbc7c39bae7696f95e55e141129c864fd4
YCshould <2725687757@qq.com>
2025-08-18 15:59:48 +0800

    Mcp服务开发

---
+++ wu-ai-agent-mcp-server/src/test/java/com/wu/wuaiagentmcpserver/WuAiAgentMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wu.wuaiagentmcpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WuAiAgentMcpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
